7c3cc5b7a7097a12ab94261f59dd67ab69deca5d
Fabricio Moraes <fabriciomk2@gmail.com>
2023-08-17 10:55:49 -0300

    insertion of comments and adjustments to names

--- AzureSQL_connection_full_KEYVAULT.py
+++ AzureSQL_connection_full_KEYVAULT.py
@@ -3,15 +3,21 @@ from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 
 
+# Azure Key Vault and Secret Name settings
 kv_name = "KEYVAULT NAME"
-pw_secret_name = "pw SECRET NAME"
-user_secret_name = "user SECRET NAME"
-db_secret_name = "db SECRET NAME"
-urlsvr_secret_name = "server url SECRET NAME"
+pw_secret_name = "SECRET NAME FOR PASSWORD"
+user_secret_name = "SECRET NAME FOR USER"
+db_secret_name = "SECRET NAME FOR DATABASE"
+urlsvr_secret_name = "SECRET NAME FOR SERVER URL"
 kv_url = f"https://{kv_name}.vault.azure.net/"
 
+# Initialize default Azure credentials client
 credential_client_pw = DefaultAzureCredential()
+
+# Initialize the Azure Key Vault secrets client
 secret_client_pw = SecretClient(vault_url=kv_url, credential=credential_client_pw)
+
+# Get the secret value from Azure Key Vault
 pw_secret_value = secret_client_pw.get_secret(pw_secret_name).value
 
 credential_client_user = DefaultAzureCredential()
@@ -26,11 +32,13 @@ credential_client_svr = DefaultAzureCredential()
 secret_client_svr = SecretClient(vault_url=kv_url, credential=credential_client_svr)
 svr_secret_value = secret_client_svr.get_secret(urlsvr_secret_name).value
 
+# Azure SQL Database Settings
 svr_url = svr_secret_value
 db_name = db_secret_value
 username = user_secret_value
 pw = pw_secret_value
 
+# Connection string
 connection_str = (
     f'''Driver={'ODBC Driver 18 for SQL Server'};
     Server={svr_url};
@@ -42,8 +50,12 @@ connection_str = (
     Connection Timeout=30;''')
 
 try:
+    # Establish the database connection
     conn = pyodbc.connect(connection_str)
     print("Connected!")
+    
+    # Close the connection
+    conn.close()
     
 except:
     raise Exception
