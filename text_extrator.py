af65dbf8cca79b6d9cdc1daf5032efea574d8736
Fabricio Moraes <fabriciomk2@gmail.com>
2023-08-18 11:12:16 -0300

    write file to path

--- text_extrator.py
+++ text_extrator.py
@@ -1,11 +1,21 @@
 import PyPDF2
 
-pdf_file = open('FMC_SparkDeveloper.pdf', 'rb')
+
+pdf_file = open('MyDP900.pdf', 'rb')
 
 reader = PyPDF2.PdfReader(pdf_file)
 
 page_number = reader.pages[0]
 
 text = page_number.extract_text()
+lines = text.split('\n')
+selecteds = lines[1:11]
 
-print(text)
+print(selecteds)
+
+path = "YOUR PATH"
+
+with open(path, "w") as arquivo:
+    arquivo.write(text)
+
+print("Content saved with success!")
