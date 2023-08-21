1143e21ffa3168f208fc602decff7ad4f22ba879
Fabricio Moraes <fabriciomk2@gmail.com>
2023-08-21 16:37:28 -0300

    final commit

--- text_extrator.py
+++ text_extrator.py
@@ -1,21 +1,33 @@
+# Code for to extract the most important information and save as .txt file
 import PyPDF2
 
 
-pdf_file = open('MyDP900.pdf', 'rb')
+# Open PDF file on binary mode
+pdf_file = open('YOUR_FILE.pdf', 'rb')
 
+# Read to memory
 reader = PyPDF2.PdfReader(pdf_file)
 
+# Select first page
 page_number = reader.pages[0]
 
+# Extract the text.
 text = page_number.extract_text()
-lines = text.split('\n')
-selecteds = lines[1:11]
 
+# Selecting the most important information 
+selecteds = text[2:300]
+
+# Check the strings
 print(selecteds)
 
-path = "YOUR PATH"
+# Path to save
+path_1 = "C:/Users/ YOUR_FOLDER /Desktop/my_output.txt"
 
-with open(path, "w") as arquivo:
-    arquivo.write(text)
+# Try write the content to .txt file
+try:
+    with open(path_1, "w") as file:
+        file.write(selecteds)
+    print("Content saved with success!")
 
-print("Content saved with success!")
+except Exception as err:
+    print(err)
